abd04f6c3d0061c39a041154370055003d538bb1
URReVo <82812051+URReVo@users.noreply.github.com>
2026-09-24 08:39:22 +0200

    Experiment: strengthen Circa result success green

--- experiments/prototype/service-worker.js
+++ experiments/prototype/service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const RELEASE="73";
-const CACHE_REVISION="prototype-r9";
+const CACHE_REVISION="prototype-r10";
 const BASE="/experiments/prototype";
 const CACHE_PREFIX="imposter-games-prototype-";
 const CACHE_NAME=CACHE_PREFIX+"v"+RELEASE+"-"+CACHE_REVISION;
