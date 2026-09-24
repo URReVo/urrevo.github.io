a19c8a2f5e968f7bf162bfb2b6b4d452b7138f35
URReVo <82812051+URReVo@users.noreply.github.com>
2026-09-24 08:28:00 +0200

    Experiment: fix Classic hidden screens and opaque iOS status bar

--- experiments/prototype/service-worker.js
+++ experiments/prototype/service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const RELEASE="73";
-const CACHE_REVISION="prototype-r5";
+const CACHE_REVISION="prototype-r6";
 const BASE="/experiments/prototype";
 const CACHE_PREFIX="imposter-games-prototype-";
 const CACHE_NAME=CACHE_PREFIX+"v"+RELEASE+"-"+CACHE_REVISION;
